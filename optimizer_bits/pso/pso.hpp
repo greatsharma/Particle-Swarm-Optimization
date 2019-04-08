c7c4285aaf293964bb702a241571bb5a520daa6d
Gaurav Sharma <gsharma81999@gmail.com>
2019-04-08 12:53:52 +0530

    update pso.hpp

--- optimizer_bits/pso/pso.hpp
+++ optimizer_bits/pso/pso.hpp
@@ -20,15 +20,15 @@ public:
     * 
     * @param maxIter Maximum no of iterations.
     * @param swarmSize Swarm population size.
-    * @param intertia Inertia Coefficient.
-    * @param inertiaDamp Damping ratio of inertia coefficient.
+    * @param inertia Inertia Coefficient.
+    * @param inertiaDrop Damping ratio of inertia coefficient.
     * @param c1 Personal acceleration coeff.
     * @param c2 Global acceleration coeff.
     */
   PSO(const size_t maxIter = 1000,
       const size_t swarmSize = 50,
-      double intertia = 1.0,
-      const double inertiaDamp = 0.95,
+      double inertia = 1.0,
+      const double inertiaDrop = 0.95,
       const double c1 = 2.0,
       const double c2 = 2.0);
 
@@ -42,24 +42,60 @@ public:
   double Optimize(PSOTestFunction function,
                   double *iterate);
 
+  // Get maximum no of iterations.
+  size_t getMaxIter() const { return maxIter; }
+
+  // Modify maximum no of iterations.
+  size_t getMaxIter() { return maxIter; }
+
+  // Get swarm population size.
+  size_t getSwarmSize() const { return swarmSize; }
+
+  // Modify swarm population size.
+  size_t getSwarmSize() { return swarmSize; }
+
+  // Get inertia coeff.
+  double getInertia() const { return inertia; }
+
+  // Modify inertia coeff.
+  double getInertia() { return inertia; }
+
+  // Get damping ratio of inertia coeff.
+  double getInertiaDrop() const { return inertiaDrop; }
+
+  // Modify damping ratio of inertia coeff.
+  double getInertiaDrop() { return inertiaDrop; }
+
+  // Get personal acceleration coeff.
+  double getC1() const { return c1; }
+
+  // Modify personal acceleration coeff.
+  double getC1() { return c1; }
+
+  // Get global acceleration coeff.
+  double getC2() const { return c2; }
+
+  // Modify global acceleration coeff.
+  double getC2() { return c2; }
+
 private:
   // maximum no of iterations.
-  const int maxIter;
+  size_t maxIter;
 
   // Swarm population size.
-  const int swarmSize;
+  size_t swarmSize;
 
   // Inertia coeff.
-  double intertia;
+  double inertia;
 
   // Damping ratio of inertia coeff.
-  const double inertiaDamp;
+  double inertiaDrop;
 
   // Personal acceleration coeff.
-  const double c1;
+  double c1;
 
   // Global acceleration coeff.
-  const double c2;
+  double c2;
 }; //class PSO
 
 } // namespace optimization
